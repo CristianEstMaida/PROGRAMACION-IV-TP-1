66ad91217a9d001277401db7ebf2d65ecebc8fa1
CristianEstMaida <55913935+CristianEstMaida@users.noreply.github.com>
2026-09-16 19:08:44 -0300

    fix: corrijo estilos reset password

--- src/app/componentes/reset-password/reset-password.ts
+++ src/app/componentes/reset-password/reset-password.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { form, FormField, required } from '@angular/forms/signals';
 import { RouterLink, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 import { Auth } from '../../services/auth';
 
 @Component({
@@ -9,12 +10,14 @@ import { Auth } from '../../services/auth';
   selector: 'app-reset-password',
   templateUrl: './reset-password.html',
   styleUrls: ['./reset-password.css'],
-  imports: [FormField, RouterLink, CommonModule]
+  imports: [FormField, RouterLink, CommonModule, MatIconModule]
 })
 export class ResetPassword {
   resetModel = signal({
     password: ''
   });
+
+  showPassword = signal<boolean>(false);
 
   resetForm = form(this.resetModel, (schemaPath) => {
     required(schemaPath.password, { message: 'Nueva contraseña es obligatoria' });
@@ -22,19 +25,18 @@ export class ResetPassword {
 
   constructor(private auth: Auth, private router: Router) {}
 
+  togglePasswordVisibility() {
+    this.showPassword.update(v => !v);
+  }
+
   async onSubmit(event: Event) {
     event.preventDefault();
     const { password } = this.resetModel();
-
-    // Supabase ya te da el access_token en la URL cuando el usuario entra desde el mail
     const result = await this.auth.updatePassword(password);
-
     if (result.error) {
       console.error('Error al actualizar contraseña:', result.error);
       return;
     }
-
-    console.log('Contraseña actualizada correctamente');
     this.router.navigate(['/login']);
   }
 }
